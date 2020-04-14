d11348e707903a72b84f5fd7389e06903b8d1c94
Kaushik Tummalapalli <54365583+kaushik-42@users.noreply.github.com>
2020-04-15 00:46:18 +0530

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -29,3 +29,31 @@ the_most_dangerous_graph = {
 print(dfs(the_most_dangerous_graph, "crocodiles", "bees"))
 
 OUTPUT: ['crocodiles', 'lasers', 'sharks', 'lava', 'piranhas', 'bees']
+	
+def bfs(graph, start_vertex, target_value):
+  path = [start_vertex]
+  vertex_and_path = [start_vertex, path]
+  bfs_queue = [vertex_and_path]
+  visited = set()
+  while bfs_queue:
+    current_vertex, path = bfs_queue.pop(0)
+    visited.add(current_vertex)
+    for neighbor in graph[current_vertex]:
+      if neighbor not in visited:
+        if neighbor is target_value:
+          return path + [neighbor]
+        else:
+          bfs_queue.append([neighbor, path + [neighbor]])
+
+some_hazardous_graph = {
+    'lava': set(['sharks', 'piranhas']),
+    'sharks': set(['piranhas', 'bees']),
+    'piranhas': set(['bees']),
+    'bees': set(['lasers']),
+    'lasers': set([])
+  }
+print(bfs(the_most_dangerous_graph, "crocodiles", "bees"))
+
+OUTPUT: ['crocodiles', 'lasers', 'sharks', 'lava', 'piranhas', 'bees']	
+	
+	
